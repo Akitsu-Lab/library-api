c5ea6ecb8d7cbd579fe634522af3268e950fbad0
SOTA <snssota@gmail.com>
2022-09-10 11:17:02 +0900

    serviceクラスをインポート コード整形

--- src/main/java/com/example/libraryapi/controller/BookRestController.java
+++ src/main/java/com/example/libraryapi/controller/BookRestController.java
@@ -2,6 +2,7 @@ package com.example.libraryapi.controller;
 
 import com.example.libraryapi.domain.BookList;
 import com.example.libraryapi.domain.BookSelector;
+import com.example.libraryapi.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookRestController {
     private final BookService service;
 
-    public BookRestController(BookService service){
+    public BookRestController(BookService service) {
         this.service = service;
     }
 
     @GetMapping(path = "", produces = "application/json")
-    public BookList find(BookSelector selector){
+    public BookList find(BookSelector selector) {
         return this.service.find(selector);
     }
 }
