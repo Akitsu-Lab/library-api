113493e25a7075d42dd166c33bc55a5495e846fd
SOTA <snssota@gmail.com>
2022-09-10 17:21:05 +0900

    コード整形

--- src/main/resources/bookDBinit.sql
+++ src/main/resources/bookDBinit.sql
@@ -5,14 +5,18 @@ USE BOOK;
 
 DROP TABLE IF EXISTS book;
 
-CREATE TABLE `book`(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(128) DEFAULT Null,
-    `pages` int(11),
+CREATE TABLE `book`
+(
+    `id`      int(11) NOT NULL AUTO_INCREMENT,
+    `title`   varchar(128) DEFAULT Null,
+    `pages`   int(11),
     `content` TEXT,
     PRIMARY KEY (`id`)
 );
 
-INSERT INTO book VALUES (1,'Java',332,'Javaについて');
-INSERT INTO book VALUES (2,'SpringBoot',80,'SpringBootについて');
-INSERT INTO book VALUES (3,'Git',12,'Gitについて');
+INSERT INTO book
+VALUES (1, 'Java', 332, 'Javaについて');
+INSERT INTO book
+VALUES (2, 'SpringBoot', 80, 'SpringBootについて');
+INSERT INTO book
+VALUES (3, 'Git', 12, 'Gitについて');
