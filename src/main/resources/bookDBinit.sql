c40505a01a6603037403c69ada745ffc2a4f574c
SOTA <snssota@gmail.com>
2022-09-10 13:31:41 +0900

    DBを作成するSQLを作成

---
+++ src/main/resources/bookDBinit.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS BOOK;
+CREATE DATABASE BOOK DEFAULT CHARACTER SET utf8;
+
+USE BOOK;
+
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE `book`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(128) DEFAULT Null,
+    `pages` int(11),
+    `content` TEXT,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO book VALUES (1,'Java',332,'Javaについて');
+INSERT INTO book VALUES (2,'SpringBoot',80,'SpringBootについて');
+INSERT INTO book VALUES (3,'Git',12,'Gitについて');
