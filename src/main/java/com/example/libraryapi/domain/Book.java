0e33f5762dec4d396a4715ac724a84a1bfa43291
SOTA <snssota@gmail.com>
2022-09-10 13:49:48 +0900

    book情報にpages,contentを追加

--- src/main/java/com/example/libraryapi/domain/Book.java
+++ src/main/java/com/example/libraryapi/domain/Book.java
@@ -3,6 +3,8 @@ package com.example.libraryapi.domain;
 public class Book {
     private long bookId;
     private String bookTitle;
+    private int pages;
+    private String content;
 
     //getterとsetter
     public long getBookId() {
@@ -19,5 +21,20 @@ public class Book {
 
     public void setBookTitle(String bookTitle) {
         this.bookTitle = bookTitle;
+    }
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
